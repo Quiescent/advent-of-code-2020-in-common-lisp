6fda3baa5a37122457068f290ad0873be86a96a5
Edward John Steere <edward.steere@gmail.com>
2020-12-23 08:27:41 +0200

    Works with example

---
+++ day23.lisp
@@ -0,0 +1,149 @@
+(eval-when (:execute :compile-toplevel :load-toplevel)
+  (ql:quickload '(:iterate
+                  :cl-ppcre
+                  :cl-arrows
+                  :trivia
+                  :trivia.ppcre
+                  :metabang-bind
+                  :anaphora
+                  :cl-heap))
+  (load "./file.lisp")
+  (defpackage :day23
+    (:use :common-lisp)
+    (:use :file)
+    (:use :iterate)
+    (:use :cl-ppcre)
+    (:use :cl-arrows)
+    (:use :trivia)
+    (:use :trivia.ppcre)
+    (:use :metabang-bind)
+    (:use :anaphora)
+    (:use :cl-heap))
+  (in-package :day23))
+
+#+nil
+(file-lines 23)
+
+(defun make-move (current-cup cups len)
+  (bind ((next-1 (gethash current-cup cups))
+         (next-2 (gethash next-1      cups))
+         (next-3 (gethash next-2      cups))
+         (next-4 (gethash next-3      cups))
+         (destination (iter
+                        (with dest = (1- current-cup))
+                        (initially (setf dest (mod dest len)))
+                        (while (or (= dest current-cup) (member dest (list next-1 next-2 next-3))))
+                        (decf dest)
+                        (setf dest (mod dest len))
+                        (while (or (= dest current-cup) (member dest (list next-1 next-2 next-3))))
+                        (finally (return dest)))))
+    ;; Could the destination be the current cup?
+    (setf (gethash current-cup cups) next-4)
+    (setf (gethash next-3 cups)      (gethash destination cups))
+    (setf (gethash destination cups) next-1)
+    (gethash current-cup cups)))
+
+#+nil
+'(5 3 2 6 0 8 1 4 7)
+
+#+nil
+'(2 7 8 0 1 4 3 5 6)
+
+(defun part-1 ()
+  (bind ((first-ordering (map 'vector #'identity '(5 3 2 6 0 8 1 4 7)))
+         (len            (length first-ordering))
+         (cups           (make-hash-table)))
+    (iter
+      (for cup in-vector first-ordering)
+      (for i from 0)
+      (setf (gethash cup cups) (aref first-ordering (mod (1+ i) len))))
+    (iter
+      (with current-cup = 5)
+      (repeat 100)
+      (format t "~a~%" (iter
+                         (with start = current-cup)
+                         (for next = (gethash start cups))
+                         (collecting (-> (format nil "~a" (1+ next)) (aref 0)) :result-type 'string)
+                         (while (/= next current-cup))
+                         (setf start next)))
+      (setf current-cup (make-move current-cup cups len))
+      (format t "current-cup: ~a~%" current-cup)
+      (finally
+       (return
+         (iter
+           (with start = 0)
+           (for next = (gethash start cups))
+           (while (/= next 0))
+           (collecting (-> (format nil "~a" (1+ next)) (aref 0)) :result-type 'string)
+           (setf start next)))))))
+
+;; Wrong: 58619437
+;; Wrong: 82439657
+
+(defun make-move-2 (current-cup cups len)
+  (bind ((next-1 (aref cups current-cup))
+         (next-2 (aref cups      next-1))
+         (next-3 (aref cups      next-2))
+         (next-4 (aref cups      next-3))
+         (destination (iter
+                        (with dest = (1- current-cup))
+                        (initially (setf dest (mod dest len)))
+                        (while (or (= dest current-cup) (member dest (list next-1 next-2 next-3))))
+                        (decf dest)
+                        (setf dest (mod dest len))
+                        (while (or (= dest current-cup) (member dest (list next-1 next-2 next-3))))
+                        (finally (return dest)))))
+    ;; Could the destination be the current cup?
+    (setf (aref cups current-cup) next-4)
+    (setf (aref cups next-3)      (aref cups destination))
+    (setf (aref cups destination) next-1)
+    (aref cups current-cup)))
+
+(defun part-2 ()
+  (bind ((first-ordering (map 'vector #'identity '(2 7 8 0 1 4 3 5 6)))
+         (len            1000000)
+         (cups           (make-array (list len))))
+    (iter
+      (for cup in-vector first-ordering)
+      (for i from 0 below 8)
+      (setf (aref cups cup) (aref first-ordering (mod (1+ i) len))))
+    (iter
+      (for i from 9 below len)
+      (setf (aref cups i) (1+ i)))
+    (setf (aref cups 6) 9)
+    (setf (aref cups (1- len)) 2)
+    (iter
+      (for i from 0 below 10)
+      (format t "(aref cups i): ~a~%" (aref cups i)))
+    (iter
+      (for i from (- len 10) below len)
+      (format t "(aref cups i): ~a~%" (aref cups i)))
+    ;; (format t "~a~%"
+    ;;         (iter
+    ;;           (with start = 0)
+    ;;           (for next = (aref cups start))
+    ;;           (format t "next: ~a~%" next)
+    ;;           (while (/= next 0))
+    ;;           (while (/= next 100))
+    ;;           (collecting (-> (format nil "~a" (1+ next)) (aref 0)) :result-type 'string)
+    ;;           (setf start next)))
+
+    (iter
+      ;; Change back!
+      (with current-cup = 2)
+      (repeat 10000000)
+      (setf current-cup (make-move-2 current-cup cups len))
+      ;; (finally
+      ;;  (return
+      ;;    (iter
+      ;;      (with start = 0)
+      ;;      (for next = (aref cups start))
+      ;;      (while (/= next 0))
+      ;;      (collecting (-> (format nil "~a" (1+ next)) (aref 0)) :result-type 'string)
+      ;;      (setf start next))))
+      (finally
+       (return
+         (* (print (1+ (aref cups 0)))
+            (print (1+ (aref cups (aref cups 0)))))))
+      )
+    ))
