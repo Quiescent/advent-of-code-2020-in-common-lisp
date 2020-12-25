d6adfc714c02780819b24f3c29cc9bce113e61ff
Edward John Steere <edward.steere@gmail.com>
2020-12-25 07:28:06 +0200

    Make it really, really quick

--- day25.lisp
+++ day25.lisp
@@ -24,21 +24,28 @@
 #+nil
 (file-lines 25)
 
+(declaim (optimize (speed 3) (safety 0) (debug 0)))
+
 (defun part-1 ()
-  (bind ((pub-key-1 15628416)
-         loop-size-1
-         (pub-key-2 11161639))
+  (bind ((pub-key-1   15628416)
+         (loop-size-1 0)
+         (pub-key-2   11161639))
+    (declare (type (unsigned-byte 64) pub-key-1 pub-key-2 loop-size-1))
     (iter
-      (for x from 2 below 100000000)
-      (with value = 7)
-      (setf value (mod (* value 7) 20201227))
+      (for x from 2 below 1000000)
+      (with value = (the (unsigned-byte 64) 7))
+      (declare (type (unsigned-byte 64) x value))
+      (setf value (mod (* value 7)
+                       (the (unsigned-byte 64) 20201227)))
       (when (= value pub-key-1)
         (setf loop-size-1 x)
         (finish)))
     (iter
       (for x from 1 below loop-size-1)
       (with value = pub-key-2)
-      (setf value (mod (* value pub-key-2) 20201227))
+      (declare (type (unsigned-byte 64) x value))
+      (setf value (mod (* value pub-key-2)
+                       (the (unsigned-byte 64) 20201227)))
       (finally (return value)))))
 
 ;; Wrong: 10163600
